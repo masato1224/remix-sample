1533e4ab48a50fb0aa0b50ce1050fa4ad7940056
suzuki.masato <suzuki.masato@a-tm.co.jp>
2022-09-24 13:49:48 +0900

    fin

--- remix-jokes/app/root.tsx
+++ remix-jokes/app/root.tsx
@@ -1,13 +1,8 @@
-import type { MetaFunction } from "@remix-run/node";
 import type { LinksFunction } from "@remix-run/node";
-
 import {
   Links,
   LiveReload,
-  Meta,
   Outlet,
-  Scripts,
-  ScrollRestoration,
 } from "@remix-run/react";
 
 import globalStylesUrl from "./styles/global.css";
@@ -33,23 +28,43 @@ export const links: LinksFunction = () => {
   ];
 };
 
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
-});
-
-export default function App() {
+function Document({
+  children,
+  title = `Remix: So great, it's funny!`,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
-        <Meta />
+        <meta charSet="utf-8" />
+        <title>{title}</title>
         <Links />
       </head>
       <body>
-      <Outlet />
+        {children}
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+// 60
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Uh-oh!">
+      <div className="error-container">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
   );
 }
